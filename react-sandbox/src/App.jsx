f068f397a85cab22e4fefc6b34a8093640a0079d
JoperHoper <josephinelim.sm@gmail.com>
2024-06-20 19:05:13 +0800

    This is the starting point of the website

---
+++ react-sandbox/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+// COMPONENT FUNCTION NAME -> INITIALISE
+const App = () => {
+  // STATES & VARIABLES
+  
+  //RETURN
+  return (
+    <>
+      <div className="our-first-container">Test</div>   
+    </>
+  );
+};
+export default App;
